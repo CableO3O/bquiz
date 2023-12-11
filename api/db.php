07306293bdcc9fbae5b074faedd9ebf8fb91d9c8
cable <cable870526@gmail.com>
2023-12-11 13:49:03 +0800

    新增管理這帳號功能

--- api/db.php
+++ api/db.php
@@ -155,6 +155,7 @@ $Bottom=new DB('bottom');
 $Mvim=new DB('mvim');
 $Image=new DB('image');
 $News=new DB('news');
+$Admin=new DB('admin');
 
 if (isset($_GET['do'])) {
     $DB=${ucfirst($_GET['do'])};
