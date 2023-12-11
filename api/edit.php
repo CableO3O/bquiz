78236cee8fb4f580e1cfb06e610fb52cfb4b8dc7
cable <cable870526@gmail.com>
2023-12-11 09:28:52 +0800

    動態圖功能完成

--- api/edit.php
+++ api/edit.php
@@ -1,21 +1,27 @@
-<?php
-include_once "db.php";
+<?php include_once "db.php";
 
-$table = $_POST['table'];
-$DB = ${ucfirst($table)};
+$table=$_POST['table'];
+$DB=${ucfirst($table)};
 unset($_POST['table']);
 
-foreach ($_POST['text'] as $id => $text) {
-    if (isset($_POST['del']) && in_array($id, $_POST['del'])) {
+if(isset($_POST['id'])){
+    foreach($_POST['id'] as $id){
+        $_POST['text'][$id]='';
+    }
+}
+
+foreach($_POST['text'] as $id => $text){
+    if(isset($_POST['del']) && in_array($id,$_POST['del'])){
         $DB->del($id);
-    } else {
-        $row = $DB->find($id);
-        $row['text'] = $text;
-        if ($table=='title') {
-            $row['sh'] = (isset($_POST['sh']) && $_POST['sh']==$id) ? 1 : 0;
+    }else{
+        $row=$DB->find($id);
+        if(isset($row['text'])){
+            $row['text']=$text;
+        }
+        if($table=='title'){
+            $row['sh']=(isset($_POST['sh']) && $_POST['sh']==$id)?1:0;
         }else{
-            $row['sh'] = (isset($_POST['sh']) && in_array($id,$_POST['sh'])) ? 1 : 0;
-            
+            $row['sh']=(isset($_POST['sh']) && in_array($id,$_POST['sh']))?1:0;
         }
         $DB->save($row);
     }
