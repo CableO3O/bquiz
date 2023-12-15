0fc21df653f770a408fc3edad4d7e635f74a88a8
cable <cable870526@gmail.com>
2023-12-15 10:52:37 +0800

    完成選單後端api編輯新增程式

--- back/menu.php
+++ back/menu.php
@@ -13,9 +13,9 @@
                 </tr>
                 <?php
 
-                $rows = $DB->all();
+                $rows = $DB->all(['menu_id'=>0]);
                 foreach ($rows as $row) {
-                ?>
+                    ?>
                     <tr>
                         <td>
                             <input type="text" name="text[]" value="<?= $row['text']; ?>">
@@ -23,7 +23,12 @@
                         <td>
                             <input type="text" name="href[]" value="<?= $row['href']; ?>">
                         </td>
-                        <td></td>
+                        <td>
+                            <?php
+                                $counts = $DB->count(['menu_id'=>$row['id']]);
+                                echo $counts;
+                            ?>
+                        </td>
                         <td>
                             <input type="checkbox" name="sh[]" value="<?= $row['id']; ?>" <?= ($row['sh'] == 1) ? 'checked' : ''; ?>>
                         </td>
